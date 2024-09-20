f4e1ff9cdf0d31fd0813c1531238fb9669f28b41
Emirfath5 <61555207+Emirfath5@users.noreply.github.com>
2024-09-20 17:06:19 -0400

    Update 2048GameToken.sol

--- contract/2048GameToken.sol
+++ contract/2048GameToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GameTokenContract is ERC20, Ownable {
-    constructor() ERC20("GameToken", "GTK") Ownable(msg.sender) {}
+    constructor() ERC20("GameToken", "FLIP") Ownable(msg.sender) {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
